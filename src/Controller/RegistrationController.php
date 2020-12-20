5ccf65a87d3b7492f946b5898ceac8b2edd67a94
kikk01 <hugobar@live.fr>
2020-12-20 11:25:34 +0100

    add registration feature

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Security\LoginFormAuthenticathorAuthenticator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\AuthenticatorInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/registration", name="registration")
+     */
+    public function index(
+        Request $request,
+        UserPasswordEncoderInterface $userPasswordEncoder,
+        GuardAuthenticatorHandler $guard,
+        LoginFormAuthenticathorAuthenticator $login
+    ): Response {
+        
+        $user = new User;
+
+        $form = $this->createForm(UserType::class, $user)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($userPasswordEncoder->encodePassword($user, $form->get('plainPassword')->getData()));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $guard->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $login,
+                'main'
+            );
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
