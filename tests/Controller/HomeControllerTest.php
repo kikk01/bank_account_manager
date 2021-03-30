74196f9ca2d91dc96e63ae2692a222ad55c423d4
kikk01 <hugobar@live.fr>
2021-03-30 15:39:26 +0200

    update nav

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -22,6 +22,8 @@ class HomeControllerTest extends AbstractWebTestCase
         $this->assertSelectorTextContains('ul.navbar-nav', 'Se connecter');
         $this->assertSelectorTextContains('ul.navbar-nav', 'S\'inscrire');
         $this->assertSelectorTextNotContains('ul.navbar-nav', 'Se déconnecter');
+        $this->assertSelectorTextNotContains('ul.navbar-nav', 'Mes comptes');
+        $this->assertSelectorTextNotContains('ul.navbar-nav', 'Nouveau compte');
     }
 
     public function testLoginRoute()
@@ -44,8 +46,30 @@ class HomeControllerTest extends AbstractWebTestCase
         $this->client->loginUser($users['user']);
         $this->request('/');
         $this->assertSelectorTextContains('ul.navbar-nav', 'Se déconnecter');
+        $this->assertSelectorTextContains('ul.navbar-nav', 'Mes comptes');
+        $this->assertSelectorTextContains('ul.navbar-nav', 'Nouveau compte');
         $this->assertSelectorTextNotContains('ul.navbar-nav', 'Se connecter');
         $this->assertSelectorTextNotContains('ul.navbar-nav', 'S\'inscrire');
+    }
+
+    public function testBankAccountListRoute()
+    {
+        $users = $this->loadFixtureFiles([dirname(__DIR__).'/fixtures/user.yaml']);
+        $this->client->loginUser($users['user']);
+        $this->request('/');
+        $this->client->clickLink('Mes comptes');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains('h1', 'Liste des comptes');
+    }
+
+    public function testBankAccountCreatetRoute()
+    {
+        $users = $this->loadFixtureFiles([dirname(__DIR__).'/fixtures/user.yaml']);
+        $this->client->loginUser($users['user']);
+        $this->request('/');
+        $this->client->clickLink('Nouveau compte');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains('h1', 'Nouveau compte');
     }
 
     public function testLogoutRoute()
