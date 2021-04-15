00cc3755049a9c67b577c379e7f03b9e1a631256
kikk01 <hugobar@live.fr>
2021-04-15 17:42:30 +0200

    refactor test create pathConstant

--- tests/Controller/functional/BankAccount/BankAccountCreateControllerTest.php
+++ tests/Controller/functional/BankAccount/BankAccountCreateControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\BankAccount\Controller;
 
 use App\Tests\AbstractWebTestCase;
+use App\Tests\PathConstant;
 use Liip\TestFixturesBundle\Test\FixturesTrait;
 
 class BankAccountCreateControllerTest extends AbstractWebTestCase
@@ -12,32 +13,32 @@ class BankAccountCreateControllerTest extends AbstractWebTestCase
     public function testDisplayAccountCreate()
     {
         $this->loadUserFixturesThenLogin('user');
-        $this->assertDisplay('/bank-account/create', 'Nouveau compte');
+        $this->assertDisplay(PathConstant::BANK_ACCOUNT_CREATE, 'Nouveau compte');
     }
 
     public function testUserNotConnected()
     {
-        $this->request('/bank-account/create');
-        $this->assertResponseRedirects('/login');
+        $this->request(PathConstant::BANK_ACCOUNT_CREATE);
+        $this->assertResponseRedirects(PathConstant::LOGIN);
     }
 
     public function testSuccessfullBankAccountAdd()
     {
         $this->loadUserFixturesThenLogin('user');
-        $crawler = $this->request('/bank-account/create');
+        $crawler = $this->request(PathConstant::BANK_ACCOUNT_CREATE);
         $form = $crawler->selectButton('valider')->form([
             'bank_account' => [
                 'name' => 'compte courant',
                 'accountNumber' => '12345123456789012',
             ]
         ]);
-        $this->submitThenRedirect($form, BankAccountListControllerTest::PATH);
+        $this->submitThenRedirect($form, PathConstant::BANK_ACCOUNT_LIST);
     }
 
     public function testInvalidBankAccountAdd()
     {
         $this->loadUserFixturesThenLogin('user');
-        $crawler = $this->request('/bank-account/create');
+        $crawler = $this->request(PathConstant::BANK_ACCOUNT_CREATE);
         $form = $crawler->selectButton('valider')->form([
             'bank_account' => [
                 'name' => 'compte courant',
