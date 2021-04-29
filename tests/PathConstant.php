b6875cb08a37affe92e69ad430b50bfa35408059
kikk01 <hugobar@live.fr>
2021-04-29 16:14:51 +0200

    add fixtures for end to end test
    update readme
    refactor end to end test

--- tests/PathConstant.php
+++ tests/PathConstant.php
@@ -8,7 +8,8 @@ class PathConstant
     const LOGIN = '/login';
     const REGISTRATION = '/registration';
     const MOVEMENT_LIST =  '/bank-account/1/movement/list';
-    const MOVEMENT_LIST_END_TEST =  '/bank-account/9/movement/list';
+    const MOVEMENT_LIST_END_TEST =  '/bank-account/2/movement/list';
+    const MOVEMENT_LIST_END_TEST_WITHOUT_CATEGORIES =  '/bank-account/1/movement/list';
     const MOVEMENT_CREATE = '/movement/create';
     const BANK_ACCOUNT_LIST = '/bank-account/list';
     const BANK_ACCOUNT_CREATE = '/bank-account/create';
