bf42c34bf9a42093ff5cc2276c9d9709d28d892e
kikk01 <hugobar@live.fr>
2021-11-19 21:39:46 +0100

    update to symfony 5.3 use  UserPasswordHasher

--- project/src/DataFixtures/AppFixtures.php
+++ project/src/DataFixtures/AppFixtures.php
@@ -10,27 +10,27 @@ use BankAccount;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    private $passwordEncoder;
+    private $passwordHasher;
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
-        $this->passwordEncoder = $passwordEncoder;
+        $this->passwordHasher = $passwordHasher;
     }
 
     public function load(ObjectManager $manager)
     {
         $userWithoutCategories = (new User)->setEmail('userwithoutcategories@test.fr');
-        $userWithoutCategories->setPassword($this->passwordEncoder->encodePassword($userWithoutCategories, '00000000'));
+        $userWithoutCategories->setPassword($this->passwordHasher->hashPassword($userWithoutCategories, '00000000'));
         $bankAccount = (new EntityBankAccount)->setName('personnal account')->setUser($userWithoutCategories);
         $movement = (new Movement)->setAmount(100)->setDate(new DateTime())->setDescription('test')->setBankAccount($bankAccount);
         $manager->persist($movement);
 
         $user = (new User)->setEmail('test@test.fr');
-        $user->setPassword($this->passwordEncoder->encodePassword($user, '00000000'));
+        $user->setPassword($this->passwordHasher->hashPassword($user, '00000000'));
         $category = (New Category)->setName('category new')->setUser($user);
         $bankAccount = (new EntityBankAccount)->setName('personnal account')->setUser($user);
         $movement = (new Movement)
